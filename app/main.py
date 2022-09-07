ff9694693a15a735e884ede63491dbdd4a4e5b10
favss <Jonrayfavoritoytube@gmail.com>
2022-09-07 08:57:21 +0800

    add cors middleware to allow communication with frontend

--- app/main.py
+++ app/main.py
@@ -1,8 +1,21 @@
 from tabnanny import check
 from fastapi import FastAPI
 from .routers import tags, users, token, checkins
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:8080"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 app.include_router(tags.router)
 app.include_router(users.router)
